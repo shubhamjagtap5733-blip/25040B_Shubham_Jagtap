7884b0554ca5fcfe1bab01868e966bed7a570e7c
shubham jagtap <shubhamjagtap5733@gmail.com>
2026-08-03 11:27:44 +0530

    Delete 25040B_Shubham_Car Black directory

--- 25040B_Shubham_Car Black/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef UART_H
-#define UART_H
-void init_con_uart();
-void char_transmit_uart(unsigned char ch);
-void string_transmit_uart(unsigned char* str);
-
-
-unsigned char uart_recieve();
-#endif
